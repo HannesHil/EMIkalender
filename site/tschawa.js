431b27b73fe4a2db0b1d3c697de7e664aab4ff7b
Affenselfie <maximilian.kindt@mailbox.tu-dresden.de>
2014-12-05 23:24:03 +0100

    optimized javascript, mouseovers and onclicks now working correctly

--- site/tschawa.js
+++ site/tschawa.js
@@ -1,7 +1,57 @@
 /**
  * @author Maximilian
  */
-function activated(){
-	var x=document.getElementById("monat");
-	x.style.backgroundColor="#E9E9E9";
+	var x = document.getElementById("monat");
+	var y = document.getElementById("wochen");
+	var z = document.getElementById("uebersicht");
+    var xactive=true;
+    var yactive=true;
+    var zactive=true;
+function activatedMonat() {
+var x = document.getElementById("monat");
+	var y = document.getElementById("wochen");
+	var z = document.getElementById("uebersicht");
+	
+	x.style.backgroundColor = "#E9E9E9"; xactive = false;
+	y.style.backgroundColor = "#FFFFFF"; yactive = true;
+	z.style.backgroundColor = "#FFFFFF"; zactive = true;
+}
+
+function activatedWoche() {
+var x = document.getElementById("monat");
+	var y = document.getElementById("wochen");
+	var z = document.getElementById("uebersicht");
+
+	x.style.backgroundColor = "#FFFFFF"; xactive=true;
+	y.style.backgroundColor = "#E9E9E9"; yactive=false;
+	z.style.backgroundColor = "#FFFFFF"; zactive=true;
+}
+function activatedUebersicht(){
+var x = document.getElementById("monat");
+	var y = document.getElementById("wochen");
+	var z = document.getElementById("uebersicht");
+
+	x.style.backgroundColor = "#FFFFFF"; xactive=true;
+	y.style.backgroundColor = "#FFFFFF"; yactive=true;
+	z.style.backgroundColor = "#E9E9E9"; zactive=false;
+}
+
+function sickeFaerbung(a){
+	
+	a.style.backgroundColor="E9E9E9";
+}
+function sickeEntfaerbungx (a) {
+  if (xactive)
+  a.style.backgroundColor="#FFFFFF";
+  
+}
+function sickeEntfaerbungy (a) {
+  if (yactive)
+  a.style.backgroundColor="#FFFFFF";
+  
+}
+function sickeEntfaerbungz (a) {
+  if (zactive)
+  a.style.backgroundColor="#FFFFFF";
+  
 }
