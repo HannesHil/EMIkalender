a298a26f8d5effb85d52dcdcbb73794af97dc36e
HannesHil <jahnlog@aol.de>
2014-12-07 13:57:04 +0100

    add Comments
    bei weiteren Fragen montag 4 DS.

--- site/tschawa.js
+++ site/tschawa.js
@@ -4,37 +4,38 @@
  
  
 	
- var objekte = new Array();
-	var x = document.getElementById("monat");
-	var y = document.getElementById("wochen");	
-	var z = document.getElementById("uebersicht");  
-	var xxx = document.getElementById("ViewPane");
-
-		objekte[0] = x;
-		objekte[1] = y;
-		objekte[2] = z;
+ 	var objekte = new Array(); 				//neues Array
 	
-function activateSelektor(a) {	
-	for	(i = 0; i < objekte.length; i++) {
-		if(objekte[i].id == a.id){
-		objekte[i].className = 'selektor_choosen';
-		var htmldatei = '' + a.id + '.html';
-		xxx.src  = htmldatei;
+	var x = document.getElementById("monat");		//x wird ausgelesen
+	var y = document.getElementById("wochen");		//y wird ausgelesen
+	var z = document.getElementById("uebersicht");  	//z wird ausgelesen
+	var iFramE = document.getElementById("ViewPane");		//variable für den IFrame
+
+		objekte[0] = x;					// x in array
+		objekte[1] = y;					// y in array
+		objekte[2] = z;					// z in array
+	
+function activateSelektor(a) {					//die funktion nimmt einen Parameter a	
+	for	(i = 0; i < objekte.length; i++) {		// eine schleife durchläuft das Array objekt
+		if(objekte[i].id == a.id){			// abfrage ob die id des objektes == id array[i]
+		objekte[i].className = 'selektor_choosen';	//wenn die abfrage = true dann ändere die Klasse
+		var htmldatei = '' + a.id + '.html';		//erstellen des HTml-Datei-Namens
+		iFramE.src  = htmldatei;				// ändern des iframes fenster
 	
 		}else{
-		objekte[i].className = 'selektor_unchoosen';
+		objekte[i].className = 'selektor_unchoosen';	//wenn nicht ausgewählt dann set unchoosen
 		}	
 	}	
 }
 
-function sickeFaerbung(a){
+function sickeFaerbung(a){					//funktionskack
 
-	if(a.className != 'selektor_choosen')
+	if(a.className != 'selektor_choosen')			//wenn nicht ausgewählt dann set hoover
 	a.className = 'selektor_hoover';	
 }
 
 function sickeEntfaerbung (a) {
-	if(a.className == 'selektor_hoover')
+	if(a.className == 'selektor_hoover')			// wenn gehoovert dann set unchoosen
 	a.className = 'selektor_unchoosen';  
 }
 
