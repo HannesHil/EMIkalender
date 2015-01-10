f3279fa1dae8c25551d7c1bed1e07d5429dc6f8e
Affenselfie <maximilian.kindt@mailbox.tu-dresden.de>
2015-01-10 16:34:26 +0100

    fade function eingefügt

--- site/tschawa.js
+++ site/tschawa.js
@@ -41,7 +41,33 @@ function sickeEntfaerbung (a) {
 }
 
 
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer); 
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+       
+    }, 10);
+}
 
+function fade(element){
+	var op =element.style.opacity;
+	element.style.display = 'block';
+	var timer =setInterval(function(){
+		if (op <=0.1){
+			clearInterval(timer);
+		};
+		element.style.opacity=op;
+		element.style.filter= 'alpha(opacity=' + op*100+ ")";
+		op -= op *0.1;
+		
+	},20);
+}
 
 
 
